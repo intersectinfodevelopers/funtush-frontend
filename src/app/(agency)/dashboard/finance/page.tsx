e1b975a8f60634289fbee92ee60a2526d6e30cfc
Rajesh Khatiwada <rajeshkhatiwada99@gmail.com>
2026-06-28 15:03:50 +0545

    feat: add recent transactions table

--- src/app/(agency)/dashboard/finance/page.tsx
+++ src/app/(agency)/dashboard/finance/page.tsx
@@ -2,18 +2,18 @@
 
 import finance from '../../../../../data/finance.json';
 import SummaryStatCard from '@/components/agency/finance/SummaryStatCard';
+import RecentTransactions from '@/components/agency/finance/RecentTransactions';
 
 const agencyId = 'ag-001';
 
 const isAgencyData = (agency_id: string) => agency_id === agencyId;
 
-const totalIncome = finance.income
-  .filter((inc) => isAgencyData(inc.agency_id))
-  .reduce((sum, inc) => sum + inc.amount, 0);
+const incomeArr = finance.income.filter((inc) => isAgencyData(inc.agency_id));
+const expensesArr = finance.expenses.filter((expense) => isAgencyData(expense.agency_id));
 
-const totalExpenses = finance.expenses
-  .filter((expense) => isAgencyData(expense.agency_id))
-  .reduce((sum, expense) => sum + expense.amount, 0);
+const totalIncome = incomeArr.reduce((sum, inc) => sum + inc.amount, 0);
+
+const totalExpenses = expensesArr.reduce((sum, expense) => sum + expense.amount, 0);
 
 const netProfit = totalIncome - totalExpenses;
 
@@ -30,6 +30,8 @@ export default function AgencyFinancePage() {
         netProfit={netProfit}
         outstandingInvoices={ouststandingInvoice}
       />
+
+      <RecentTransactions incomeArr={incomeArr} expensesArr={expensesArr} />
     </div>
   );
 }
